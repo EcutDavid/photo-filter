35366cff56fd99e3bf57fb5a930f80f5991ea849
David Guan <davidguandev@gmail.com>
2017-09-02 19:27:24 +1000

    PIXI skip hello

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
 import 'babel-polyfill';
+import * as PIXI from 'pixi.js';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './components/Main';
 
+PIXI.utils.skipHello();
 import 'normalize.css/normalize.css';
 
 import 'styles/reset.scss';
